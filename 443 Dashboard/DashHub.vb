8fd4f6a3b93201800a5c1c5fadb1c4ff1bfd4584
Steve Kelly <kellyst@mail.gvsu.edu>
2015-03-31 19:44:26 -0400

    Hub progress

---
+++ 443 Dashboard/DashHub.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.AspNet.SignalR.Client.Hubs
+Imports Microsoft.AspNet.SignalR
+
+
+Public Class DashHub
+    Inherits Hub
+    Public Sub BroadcastMessageToAll(message As String)
+        Clients.All.newMessageReceived(message)
+    End Sub
+
+    Public Sub JoinAGroup(group As String)
+        Groups.Add(Context.ConnectionId, group)
+    End Sub
+
+    Public Sub RemoveFromAGroup(group As String)
+        Groups.Remove(Context.ConnectionId, group)
+    End Sub
+
+    Public Sub BroadcastToGroup(message As String, group As String)
+        Clients.Group(group).newMessageReceived(message)
+    End Sub
+End Class
