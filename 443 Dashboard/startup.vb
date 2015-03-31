23448b2aa80d3004eedbf0da385f2b6a421bffca
stokfisk <stokfisk@mail.gvsu.edu>
2015-03-31 19:40:53 -0400

    OWIN Startup Class

---
+++ 443 Dashboard/startup.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.AspNet.SignalR
+Imports Microsoft.Owin
+Imports Owin
+<Assembly: OwinStartup(GetType(dashboard.startup))> 
+Namespace dashboard
+    Public Class startup
+        Public Sub Configuration(app As IAppBuilder)
+            ' Any connection or hub wire up and configuration should go here
+            app.MapSignalR()
+        End Sub
+    End Class
+End Namespace
