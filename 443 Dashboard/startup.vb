6895407029d14a90cf6215f01c297e6c1a3dd502
stokfisk <stokfisk@mail.gvsu.edu>
2015-03-31 20:34:13 -0400

    owin startup class version 2.0 - DONT COMMIT OVER THIS.

---
+++ 443 Dashboard/startup.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.Owin
+Imports Owin
+<Assembly: OwinStartup(GetType(dashboard.Startup))> 
+Namespace dashboard
+    Public Class Startup
+        Public Sub Configuration(app As IAppBuilder)
+            ' HUB CONNECTION GOES HERE
+            app.MapSignalR()
+        End Sub
+    End Class
+End Namespace
