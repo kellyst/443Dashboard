1c6602fd0dc975945a1d00025cfce45943516958
Trey Jardine <jardinet@mail.gvsu.edu>
2015-04-06 10:16:14 -0700

    Some progress

--- 443 Dashboard/JSONTest.aspx.vb
+++ 443 Dashboard/JSONTest.aspx.vb
@@ -1,5 +1,6 @@
-﻿
-Imports Newtonsoft.Json
+﻿Imports Newtonsoft.Json
+Imports System.IO
+Imports Microsoft.AspNet.SignalR
 
 Public Class JSONTest
     Inherits System.Web.UI.Page
@@ -7,9 +8,28 @@ Public Class JSONTest
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Dim webClient As New System.Net.WebClient
-        Dim result As String = webClient.DownloadString("http://api.nytimes.com/svc/news/v3/content/nyt/all[/time-period][.json]?api-key=d657c0a64ae6788a338a7f090ec5d7cc:7:71710801")
+        Dim jsonDB As New Dictionary(Of String, )
+        Dim jsStream As String = webClient.DownloadString("http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=d657c0a64ae6788a338a7f090ec5d7cc:7:71710801")
+        'Dim jsReader As StreamReader = New StreamReader(jsStream)
+        'Dim jsonread As JsonTextReader = New JsonTextReader(jsReader)
+        'Got to a point where I can seperate the different properties using the class below.. But I have not been able to manipulate the sub arrays such as 
+        'results -> title, author, url. 
+        Dim obj As json_result
+        obj = JsonConvert.DeserializeObject(Of json_result)(jsStream)
 
-        lblTest.Text = result
+        lblTest.Text = obj.ToString()
     End Sub
+End Class
 
+Public Class json_result
+    Public Property status As String
+    Public Property num_results As Integer
+    Public Property title As String
+    Public Property results As String
+    Public Property author As String
+    Public Property url As String
+
+    Public Overrides Function ToString() As String
+        Return results & " : " & title & status & num_results & " : " & author & " : " & url
+    End Function
 End Class
