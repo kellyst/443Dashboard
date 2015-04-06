ba308b7a57619957c3a4086c41cb152e68ff672d
Trey Jardine <jardinet@mail.gvsu.edu>
2015-04-06 07:51:49 -0700

    JSON Most Popular

---
+++ 443 Dashboard/JSONTest.aspx.vb
@@ -0,0 +1,15 @@
+﻿
+Imports Newtonsoft.Json
+
+Public Class JSONTest
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim webClient As New System.Net.WebClient
+        Dim result As String = webClient.DownloadString("http://api.nytimes.com/svc/news/v3/content/nyt/all[/time-period][.json]?api-key=d657c0a64ae6788a338a7f090ec5d7cc:7:71710801")
+
+        lblTest.Text = result
+    End Sub
+
+End Class
